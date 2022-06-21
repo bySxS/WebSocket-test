666a61a1dd8055d1d771c704b01d563714038529
bySxS <88063532+bySxS@users.noreply.github.com>
2022-06-21 17:32:09 +0300

    Initial commit

---
+++ websocket-client.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const { io } = require("socket.io-client");
+const ss = require('socket.io-stream');
+const socket = io.connect("ws://localhost:5000/get-file");
+const filename = './file/basegame_4_gamedata.archive'
+socket.on("connect_error", (err) => {
+    console.log('error', err)
+});
+
+const statsFile = fs.statSync(filename)
+const fileSize = statsFile.size// / (1024*1024);
+
+const stream = ss.createStream();
+ss(socket).emit('big-file', stream, {name: filename});
+let blobStream = fs.createReadStream(filename)
+let size = 0;
+
+blobStream.on('data', function(chunk) {
+    size += chunk.length;
+    console.log(Math.floor(size / fileSize * 100) + '%');
+    // -> e.g. '42%'
+});
+
+blobStream.pipe(stream)
+
+//принять файл
+// ss(socket).on('big-file', (stream, data) => {
+//     const filename = './file2/basegame_4_gamedata.archive'
+//     stream.pipe(fs.createWriteStream(filename))
+// });
---
+++ websocket-server.js
@@ -0,0 +1,23 @@
+const PORT = 5000
+const fs = require('fs')
+const { Server } = require("socket.io");
+
+const io = new Server(PORT, { /* options */ });
+const ss = require('socket.io-stream')
+const filename = './file2/basegame_4_gamedata.archive'
+
+io.of('/get-file').on('connection', (socket) => {
+
+    console.log('opened connection')
+
+        //отправить
+    // const stream = ss.createStream();
+    // ss(socket).emit('big-file', stream, {name: filename});
+    // fs.createReadStream(filename).pipe(stream);
+
+    ss(socket).on('big-file', (stream, data) => {
+        stream.pipe(fs.createWriteStream(filename))
+    });
+
+})
+
