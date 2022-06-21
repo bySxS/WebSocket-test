8ee81f85408382c34f735045ca776e5da15148e2
bySxS <88063532+bySxS@users.noreply.github.com>
2022-06-21 19:39:38 +0300

    upd

--- websocket-client.js
+++ websocket-client.js
@@ -1,28 +1,43 @@
 const fs = require('fs')
-const { io } = require("socket.io-client");
+const {io} = require("socket.io-client");
 const ss = require('socket.io-stream');
 const socket = io.connect("ws://localhost:5000/get-file");
-//файл который нужно передать серверу
-const filename = './file/basegame_4_gamedata.archive'
+
 socket.on("connect_error", (err) => {
     console.log('error', err)
 });
+// const stream = ss.createStream();
 
-const statsFile = fs.statSync(filename)
-const fileSize = statsFile.size// / (1024*1024);
 
-const stream = ss.createStream();
-ss(socket).emit('big-file', stream, {name: filename});
-let blobStream = fs.createReadStream(filename)
-let size = 0;
+socket.on('connect', () => {
 
-blobStream.on('data', function(chunk) {
-    size += chunk.length;
-    console.log(Math.floor(size / fileSize * 100) + '%');
-    // -> e.g. '42%'
-});
+    console.log('connect client')
 
-blobStream.pipe(stream)
+    ss(socket).on('big-file', (stream, data) => {
+
+        let size = 0
+        let lastProc = -1
+        let Proc = 0
+        stream.on('data', function (chunk) {
+            size += chunk.length
+            Proc = Math.floor(size / data.fileSize * 100)
+            if (lastProc !== Proc) {
+                lastProc = Proc
+                console.log(Proc + '%')
+            }
+            if (100 === Proc) {
+                socket.emit('success', {response: 'success, data received!'})
+                socket.close()
+            }
+        })
+
+        stream.pipe(fs.createWriteStream(data.fileName))
+
+    })
+
+
+})
+
 
 //принять файл
 // ss(socket).on('big-file', (stream, data) => {
--- websocket-server.js
+++ websocket-server.js
@@ -1,24 +1,43 @@
 const PORT = 5000
 const fs = require('fs')
-const { Server } = require("socket.io");
-
-const io = new Server(PORT, { /* options */ });
+const {Server} = require("socket.io");
+const io = new Server(PORT, { /* options */});
 const ss = require('socket.io-stream')
-//путь файла куда сохранить от клиента
-const filename = './file2/basegame_4_gamedata.archive'
 
 io.of('/get-file').on('connection', (socket) => {
 
     console.log('opened connection')
 
-        //отправить
-    // const stream = ss.createStream();
-    // ss(socket).emit('big-file', stream, {name: filename});
-    // fs.createReadStream(filename).pipe(stream);
+    const stream = ss.createStream();
+    const filename = './file/test1.txt'
+    const statsFile = fs.statSync(filename)
+    const fileSize = statsFile.size
 
-    ss(socket).on('big-file', (stream, data) => {
-        stream.pipe(fs.createWriteStream(filename))
+    ss(socket).emit('big-file', stream, {
+        fileName: './file/test2.txt',
+        fileSize
     });
 
-})
 
+    let blobStream = fs.createReadStream(filename)
+
+    let size = 0
+    let lastProc = -1
+    let Proc = 0
+    blobStream.on('data', function (chunk) {
+        size += chunk.length;
+        Proc = Math.floor(size / fileSize * 100)
+        if (lastProc !== Proc) {
+            lastProc = Proc
+            console.log(Proc + '%');
+        }
+
+    });
+
+    blobStream.pipe(stream)
+
+    socket.on('success', (data) => {
+        console.log(data)
+    })
+
+})
